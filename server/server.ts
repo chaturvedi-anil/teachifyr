606a6515b1cfd76862b377dd4f4a1598692c7898
chaturvedi-anil <chaturvedia560@gmail.com>
2025-08-24 00:22:14 +0530

    Setup mongodb, redis and cloudary

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { app } from "./app";
+import connectDB from "./utils/db";
 
 app.listen(process.env.PORT, () => {
   console.log(`Express running with PORt ${process.env.PORT}`);
+  connectDB();
 });
